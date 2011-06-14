ae8c17160c086509cf9a8185f4bf8f05368d63e9
artlogic <artlogic@gmail.com>
2011-06-14 03:47:00 -0400

    Killing express since we shouldn't need it.

--- web.js
+++ web.js
@@ -1,21 +1,28 @@
-var express = require('express');
+var static = require('node-static'),
+    http = require('http'),
+    io = require('socket.io'),
+    // Create a node-static server instance to serve the './public' folder
+    file = new(static.Server)('./public'),
+    // port based on environment (if exists)
+    port = process.env.PORT || 5000;
 
-var app = express.createServer(express.logger());
-
-app.use(express.static(__dirname + '/public')); 
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-	console.log("Listening on " + port);
+// setup static hosting
+var server = http.createServer(function (request, response) {
+    request.addListener('end', function () {
+	// Serve files!
+	file.serve(request, response);
     });
+})
+
+server.listen(port, function(){
+    console.log("Listening on " + port);
+});
 
 // socket.io
-var io = require('socket.io');
-var socket = io.listen(app);
-
+var socket = io.listen(server);
 socket.on('connection', function (client) {
-	client.on('message', function (message) {
-		console.log('Received message from client!', message);
-		client.send(message);
-	    });
+    client.on('message', function (message) {
+	console.log('Received message from client!', message);
+	client.send(message);
     });
+});
