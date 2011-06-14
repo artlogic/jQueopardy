3bf7c771f1ef92ff2c862ab19b2f4f8c202cb07b
artlogic <artlogic@gmail.com>
2011-06-14 02:23:40 -0400

    Setting up static hosting.

--- web.js
+++ web.js
@@ -2,11 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	response.send('Hello World!');
-    });
+app.use(express.static(__dirname + '/public')); 
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 	console.log("Listening on " + port);
+    });
+
+// socket.io
+var io = require('socket.io');
+var socket = io.listen(app);
+
+socket.on('connection', function (client) {
+	client.on('message', function (message) {
+		console.log('Received message from client!', message);
+		socket.send(message);
+	    });
     });
